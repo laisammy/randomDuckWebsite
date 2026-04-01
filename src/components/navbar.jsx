c7bb5c498058bbc35ecae023610b8f0f4991f555
hei hei OK OK! <98307235+laisammy@users.noreply.github.com>
2026-04-02 00:34:45 +0100

    Added navigation bar component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,21 @@
 export default function Navbar() {
     return(
-        <div className="bg-amber-300 h-full flex flex-row justify-center">
-            <img src="/duckiverseLogo.png" className="h-1/18 w-1/18"></img>
-            <h1 className="text-stone-800 text-2xl font-climate">
-                Duckiverse
-            </h1>
-        </div>
+        <nav className="bg-linear-to-b from-amber-300 to-amber-200 h-full flex flex-row justify-center gap-20">
+            <div className="flex flex-row items-center">
+                <img src="/duckiverseLogo.png" className="w-17 h-auto"></img>
+                <div className="text-stone-800 text-3xl font-climate m-4">
+                    Duckiverse
+                </div>
+            </div>
+            <div className="flex gap-8 font-lexend text-stone-700 m-4 text-2xl">
+                <a href="/" className="hover:text-stone-900">
+                    Home
+                </a>
+                
+                <a href="/gallery" className="hover:text-stone-900">
+                    Gallery
+                </a>
+            </div>
+        </nav>
     )
 }
