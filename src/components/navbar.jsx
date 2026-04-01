9f7f43778db8c9bbb936520e4c90ffd464c674e8
hei hei OK OK! <98307235+laisammy@users.noreply.github.com>
2026-04-01 23:57:16 +0100

    Initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return(
+        <div className="bg-amber-300 h-full flex flex-row justify-center">
+            <img src="/duckiverseLogo.png" className="h-1/18 w-1/18"></img>
+            <h1 className="text-stone-800 text-2xl font-climate">
+                Duckiverse
+            </h1>
+        </div>
+    )
+}
